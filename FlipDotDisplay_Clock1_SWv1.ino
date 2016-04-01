419c74198c96546d3f631ea18f20cecd7e9ac295
Harrison Shutan <hshutan@gmail.com>
2016-04-01 17:02:36 -0500

    Add time zone handling, better RTC setting method
    Using JChristensen’s Timezone.h code to provide timezone handling in
    the clock. Modified code required to display new correct time. Added
    Serial listen function for simple time (UTC) setting. The ‘menu’ system
    of the clock is still not complete.

--- FlipDotDisplay_Clock1_SWv1.ino
+++ FlipDotDisplay_Clock1_SWv1.ino
@@ -2,13 +2,13 @@
 /*
  * 45x7 Flip Dot Matrix Clock
  * Clock version/type: 1
- * 
+ *
  * HLS 2015-12-24
  *
  * Dev'd on Arduino Pro Mini 8MHz (Like UNO)
  * Runs on (Rev 1) of my 45x7 controller board.
  * Planned for Teensy 3.2
- * 
+ *
  * UNO's hardware SPI pins:
  * 11, MOSI, SER on my board
  * 12, MISO, Not used for me.
@@ -22,9 +22,20 @@
 #include <Time.h> // PJRC's Arduino time class.
 #include <Wire.h>  // Uses default SDA/SCL pins.
 #include <DS1307RTC.h>  // a basic DS1307 library that returns time as a time_t
+#include <Timezone.h>    //https://github.com/JChristensen/Timezone
 //#include <Metro.h> // A handy timer-job utility class.
 
 //Metro metroWriteDisplay = Metro(500);         // ms
+
+//US Central Time Zone (Chicago, Houston)
+TimeChangeRule usCDT = {"CDT", Second, dowSunday, Mar, 2, -300};
+TimeChangeRule usCST = {"CST", First, dowSunday, Nov, 2, -360};
+Timezone usCT(usCDT, usCST);
+TimeChangeRule *tcr;        //pointer to the time change rule, use to get the TZ abbrev
+time_t local;
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
 
 unsigned int onPulseLengthMicros = 200;  // 1000 = 1ms
 unsigned int offPulseLengthMicros = 250; // 1000 = 1ms
@@ -39,18 +50,36 @@ time_t lastTime = 0;
 
 mcp mcp(10); // Create a new Matrix CoProcessor and tell it that pin 10 is the latching clock pin.
 
+const byte button1 = 3;
+const byte button2 = 2;
+const byte button3 = 5;
+const byte button4 = 4;
+
 void setup()
 {
+  Serial.begin(9600);
+
+  Serial.println("SYSTEM TIME SET PROCEDURES:");
+  Serial.println("  Send a Universal Coordinated Time (UTC) value via this serial line.");
+  Serial.println("  The value must be followed by a newline character, which signals");
+  Serial.println("  to the microntroller a SAVE and EXEC instructions.");
+  Serial.println("DO NOT SEND ANY OTHER VALUES, STRINGS, OR DATA TO THIS SYSTEM.");
+  Serial.println("");
+
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(button3, INPUT_PULLUP);
+  pinMode(button4, INPUT_PULLUP);
+
+
   //setTime(23, 58, 35, 22, 12, 2015); // Useful for testing specific time cases.
   setSyncProvider(RTC.get); // Set time from RTC on boot up.
-  setSyncInterval(3600); // Re-read time from RTC every X seconds.
+  setSyncInterval(2700); // Re-read time from RTC every 45 minutes.
 
-//  Initial clock setting routine ---- setTime(hours, minutes, seconds, days, months, years);
-//  setTime(12, 7, 39, 25, 12, 2015);
-//  RTC.set(now());
-
-
-  lastTime = now();
+  lastTime = usCT.toLocal(now(), &tcr);//now();
 
   fullDisplayClear();
 
@@ -62,8 +91,95 @@ void setup()
 
 void loop()
 {
-  if (minute(lastTime) != minute(now())) {
-    printTimeStyle1(lastTime, now());
+
+
+  if (stringComplete) {
+    Serial.println("Setting time...");
+    if (inputString.toInt() > 1450000000) {
+      RTC.set(inputString.toInt());
+      setSyncProvider(RTC.get); // Set time from RTC
+    } else {
+      Serial.println("ERROR.INVALID.DATA.");
+    }
+    inputString = "";
+    stringComplete = false;
+    Serial.println("Okay.");
+  }
+
+    // Ea loop, refresh local var to be local time, from UTC current time.
+  local = usCT.toLocal(now(), &tcr);
+  
+
+  if ((!digitalRead(button1)) && (!digitalRead(button4))) {
+    // Enter setup mode by pressing buttons 1 and 4 at the same time.
+    CleverEraseHour(lastTime);
+    CleverEraseColon();
+    CleverEraseMinute(lastTime);
+    CleverEraseAMPM(lastTime);
+
+    mcp.drawChar(1, 1, 'S', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+    mcp.drawChar(1 + 6, 1, 'e', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+    mcp.drawChar(1 + 6 + 6, 1, 't', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+    mcp.drawChar(1, 1, 'S', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+    mcp.drawChar(1 + 6, 1, 'e', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+    mcp.drawChar(1 + 6 + 6, 1, 't', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+
+    mcp.drawChar(1 + 6 + 6 + 12, 1, 'M', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+    mcp.drawChar(1 + 6 + 6 + 12, 1, 'M', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+
+    while (digitalRead(button4)) {
+      // While the user has not exited setup mode:
+      // MINUTE set mode
+      if (!digitalRead(button2)) {
+        adjustTime(-60);
+        //RTC.set(now());
+        delay(500);
+      }
+      if (!digitalRead(button3)) {
+        adjustTime(60);
+        //RTC.set(now());
+        delay(500);
+      }
+
+      if (!digitalRead(button1)) {
+        // Change to HOUR set mode
+        mcp.drawCharInverse(1 + 6 + 6 + 12, 1, 'M', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+        mcp.drawChar(1 + 6 + 6 + 12, 1, 'H', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+        mcp.drawChar(1 + 6 + 6 + 12, 1, 'H', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+
+        while (digitalRead(button4)) {
+          // While the user has not exited:
+          // HOUR set mode
+          if (!digitalRead(button2)) {
+            adjustTime(-3600);
+            //RTC.set(now());
+            delay(500);
+          }
+          if (!digitalRead(button3)) {
+            adjustTime(3600);
+            //RTC.set(now());
+            delay(500);
+          }
+        }
+        mcp.drawCharInverse(1 + 6 + 6 + 12, 1, 'H', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+        mcp.drawChar(1 + 6 + 6 + 12, 1, 'M', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+        mcp.drawChar(1 + 6 + 6 + 12, 1, 'M', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+        // This could use some improvement, technically a second button 4
+        // press will be needed to exist the larger while loop. Okay if instructions
+        // are to hold 4 to exit setup.
+      }
+    }
+    // Exit setup mode.
+    fullDisplayClear();
+    FreshWriteHour(lastTime);
+    FreshWriteColon();
+    FreshWriteMinute(lastTime);
+    FreshWriteAMPM(lastTime);
+  }
+
+
+  if (minute(lastTime) != minute(usCT.toLocal(now(), &tcr))) { //was now()
+    printTimeStyle1(lastTime, usCT.toLocal(now(), &tcr));
   }
 
   delay(1); // Slow down the loop.
@@ -99,8 +215,8 @@ time_t printTimeStyle1(time_t _lastTime, time_t _newTime) {
     FreshWriteHour(_newTime);
     FreshWriteMinute(_newTime);
     //    FreshWriteAMPM(_newTime);
-//    FreshWriteHour(_newTime);
-//    FreshWriteMinute(_newTime);
+    //    FreshWriteHour(_newTime);
+    //    FreshWriteMinute(_newTime);
     //   FreshWriteAMPM(_newTime);
   }
 
@@ -174,6 +290,10 @@ void CleverEraseHour(time_t inTime) {
   }
 }
 
+void CleverEraseColon() {
+  mcp.drawCharInverse(16, 1, ':', onPulseLengthMicros, offPulseLengthMicros, onRecoverTime, offRecoverTime, false, 1, 1);
+}
+
 
 void CleverEraseMinute(time_t inTime) {
 
@@ -211,4 +331,18 @@ void fullDisplayClear() {
   mcp.dotOn(45, 7, offPulseLengthMicros, offRecoverTime);
   //fix for weird backwards dot on my display.
   mcp.dotOn(45, 7, offPulseLengthMicros, offRecoverTime);
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
 }
